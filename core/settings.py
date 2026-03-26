7ab38248344700dffcffafde4024e95390275c3a
Miguel Ramos <return.infox@gmail.com>
2026-03-26 20:10:57 -0300

    modelo terminado

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!4hqhdw=%0bb7n$%1=ie&$8%@+w##%_52pplfnt42!f=r!3_tt'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
